bf9813898caa8c5ae0b8ad4d325706440765b790
Vladimir Pankratov <sollex.uz@gmail.com>
2025-09-11 12:04:24 +0200

    Fixed package generation

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,31 @@
-import { createConfig } from "./rollup.config.base.js";
+import { readFileSync } from "node:fs";
+import { dirname, join } from "node:path";
+import { cwd } from "node:process";
+import typescript from "@rollup/plugin-typescript";
 
-export default createConfig();
+const pkg = JSON.parse(readFileSync(join(cwd(), "package.json"), "utf8"));
+
+export default {
+  input: "guest-js/index.ts",
+  output: [
+    {
+      file: pkg.exports.import,
+      format: "esm",
+    },
+    {
+      file: pkg.exports.require,
+      format: "cjs",
+    },
+  ],
+  plugins: [
+    typescript({
+      declaration: true,
+      declarationDir: dirname(pkg.exports.import),
+    }),
+  ],
+  external: [
+    /^@tauri-apps\/api/,
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+};
